b9a99acd1c774cd3399ec1b81964dd29212f4e79
NF4Caniago <fuegoardinte@gmail.com>
2018-10-04 22:58:31 +0700

    css html

--- home.php
+++ home.php
@@ -7,8 +7,8 @@
         <div class="nav">
             <ul class="nav">
                 <li class="nav"><img class="nav" src="img/logo-bpjs.png" style="height: 50px"/></li>
-                <li class="nav"><a class="nav" href="">Home</a></li>
-                <li class="nav"><a class="nav" href="">Product</a></li>
+                <li class="nav"><a class="nav" href="home.php">Home</a></li>
+                <li class="nav"><a class="nav" href="profilUser.php">Product</a></li>
                 <li class="nav"><a class="nav" href="">Profile</a></li>
             </ul>
        </div>
